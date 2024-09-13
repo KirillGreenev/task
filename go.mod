94c7bfda0b96c2610dac44f94e14b8e041134fe0
kirill <crownbarrel@gmail.com>
2024-09-13 15:19:33 +0500

    commit 1

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module task
 
 go 1.22.0
+
+require github.com/jmoiron/sqlx v1.4.0 // indirect
--- main.go
+++ main.go
@@ -1,69 +1,121 @@
 package main
 
 import (
-	"encoding/json"
+	"context"
 	"log"
-	"net/http"
+
+	"github.com/jmoiron/sqlx"
+	_ "github.com/mattn/go-sqlite3"
 )
 
-type User struct {
-	Id        int     `json:"id"`
-	Email     string  `json:"email,omitempty"`
-	Amount    int     `json:"amount"`
-	Profile   Profile `json:"profile,omitempty"`
-	Password  string  `json:"-"`
-	Username  string  `json:"username"`
-	CreatedAt string  `json:"createdAt"`
-	CreatedBy string  `json:"createdBy"`
+type City struct {
+	ID    int    `db:"id"`
+	Name  string `db:"name"`
+	State string `db:"state"`
 }
 
-type Profile struct {
-	Avatar     string `json:"avatar,omitempty"`
-	LastName   string `json:"lastName,omitempty"`
-	FirstName  string `json:"firstName,omitempty"`
-	StaticData string `json:"-"`
+type SQLiteCityRepository struct {
+	db *sqlx.DB
+}
+
+func NewSQLiteCityRepository(db *sqlx.DB) *SQLiteCityRepository {
+	return &SQLiteCityRepository{db: db}
+}
+
+func initDB() (*sqlx.DB, error) {
+	db, err := sqlx.Open("sqlite3", "./test.db")
+	if err != nil {
+		return nil, err
+	}
+
+	createTableSQL := `CREATE TABLE IF NOT EXISTS cities (
+        id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+        name VARCHAR(30) NOT NULL,
+        state VARCHAR(30) NOT NULL
+    );`
+
+	if _, err := db.Exec(createTableSQL); err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
+
+func (repo *SQLiteCityRepository) Create(ctx context.Context, city City) (int, error) {
+	result, err := repo.db.ExecContext(ctx, "INSERT INTO cities (name, state) VALUES (?, ?)",
+		city.Name, city.State)
+	if err != nil {
+		return 0, err
+	}
+	id, err := result.LastInsertId()
+	return int(id), err
+}
+
+func (repo *SQLiteCityRepository) Delete(ctx context.Context, id int) error {
+	_, err := repo.db.ExecContext(ctx, "DELETE FROM cities WHERE id = ?", id)
+	return err
+}
+
+func (repo *SQLiteCityRepository) Update(ctx context.Context, city City) error {
+	_, err := repo.db.ExecContext(ctx, "UPDATE cities SET name = ?, state = ? WHERE id = ?",
+		city.Name, city.State, city.ID)
+	return err
+}
+
+func (repo *SQLiteCityRepository) List(ctx context.Context) ([]City, error) {
+	var cities []City
+	err := repo.db.SelectContext(ctx, &cities, "SELECT * FROM cities")
+	return cities, err
 }
 
 func main() {
-	http.HandleFunc("/user", GetUser)
+	db, err := initDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	repo := NewSQLiteCityRepository(db)
+
+	ctx := context.Background()
+
+	city := City{Name: "Москва", State: "РФ"}
+	id, err := repo.Create(ctx, city)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("Добавлен город с id: %d\n", id)
+
+	cities, err := repo.List(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Список городов:", cities)
+
+	city.ID = id
+	city.Name = "Санкт-Петербург"
+	err = repo.Update(ctx, city)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Обновлён город:", city)
+
+	err = repo.Delete(ctx, id)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Удалён город с ID:", id)
 }
 
-func GetUser(w http.ResponseWriter, _ *http.Request) {
-	users, err := GetUsers()
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-
-	for _, user := range users {
-		if user.Amount > 50000 {
-			user.Email = ""
-			user.Profile = Profile{}
-		}
-	}
-
-	json.NewEncoder(w).Encode(users)
-}
-
-func GetUsers() ([]*User, error) {
-	r, err := http.NewRequest("GET", "http://83.136.232.77:8091/users", nil)
-	if err != nil {
-		return nil, err
-	}
-
-	client := http.Client{}
-	res, err := client.Do(r)
-	if err != nil {
-		return nil, err
-	}
-	defer res.Body.Close()
-
-	var users []*User
-	if err = json.NewDecoder(res.Body).Decode(&users); err != nil {
-		return nil, err
-	}
-
-	return users, nil
-}
+/* Написать репозиторий к базе данных test на mysql, используя библиотеку sqlx.CREATE TABLE cities (
+id INTEGER NOT NULL PRIMARY KEY,
+name VARCHAR(30) NOT NULL,
+state VARCHAR(30) NOT NULL
+);
+Реализовать следующие методы:
+Create
+Delete
+Update
+List
+*/
